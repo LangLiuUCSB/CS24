f2ed416b6735925d0dcfc9388f0fe885a8bea41a
LangLiuUCSB <langliu@ucsb.edu>
2024-04-04 21:29:27 -0700

    used Prettier on censor.cpp

--- labs/welcome/code/censor.cpp
+++ labs/welcome/code/censor.cpp
@@ -1,47 +1,51 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* argv[]){
-    if(argc != 2){
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         std::cout << "USAGE: censor [length]" << std::endl;
         return 1;
     }
-
     size_t censor = std::stoi(argv[1]);
-
     std::string inSentence, outSentence;
     std::getline(std::cin, inSentence);
-
     bool prevNotSpace = inSentence[0] != ' ' || inSentence[0] != '\t';
-    size_t start = 0;
-    while(inSentence[start] == ' ' || inSentence[start] == '\t'){
-        start++;
+    size_t WordStart = 0;
+    while (inSentence[WordStart] == ' ' || inSentence[WordStart] == '\t')
+    {
+        WordStart++;
     }
     size_t wordSize = 0;
-
-    for(size_t i = start; i != inSentence.size(); i++){
-        if(inSentence[i] == ' ' || inSentence[i] == '\t'){
-            if(prevNotSpace){
-                if(wordSize != censor){
-                    outSentence.append(inSentence.substr(start, wordSize));
+    for (size_t i = WordStart; i != inSentence.size(); i++)
+    {
+        if (inSentence[i] == ' ' || inSentence[i] == '\t')
+        {
+            if (prevNotSpace)
+            {
+                if (wordSize != censor)
+                {
+                    outSentence.append(inSentence.substr(WordStart, wordSize));
                     outSentence.push_back(' ');
                 }
                 wordSize = 0;
                 prevNotSpace = false;
             }
-            start = i + 1;
-        }else{
+            WordStart = i + 1;
+        }
+        else
+        {
             wordSize++;
             prevNotSpace = true;
         }
     }
-    
-    if(prevNotSpace && wordSize != censor){
-            outSentence.append(inSentence.substr(start, wordSize));
-            outSentence.push_back(' ');
+    if (prevNotSpace && wordSize != censor)
+    {
+        outSentence.append(inSentence.substr(WordStart, wordSize));
+        outSentence.push_back(' ');
     }
     outSentence.pop_back();
     std::cout << outSentence << std::endl;
-
     return 0;
 }
