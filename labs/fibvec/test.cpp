f631525e11cd760fe8e16345ab387f14a3def97f
LangLiuUCSB <langliu@ucsb.edu>
2024-04-10 02:32:06 -0700

    reverted test.cpp to start

--- labs/fibvec/test.cpp
+++ labs/fibvec/test.cpp
@@ -8,29 +8,7 @@ int main()
 {
   FibVec fibvec;
 
-  int removeH[] = {1, 2, 3, 6, 11, 23, 47, 106, 235, 551, 1301, 3159, 7741, 19320, 48629, 123867, 317955};
-  for (int n : removeH)
-  {
-    fibvec.push(n);
-  }
-
-  std::cout << "capacity: " << fibvec.capacity() << "\n";
-  std::cout << "count: " << fibvec.count() << "\n";
-  for (unsigned long i = 0; i < fibvec.count(); i++)
-  {
-    std::cout << " " << fibvec.lookup(i);
-  }
-  std::cout << "\n";
-
-  std::cout << "removed: " << fibvec.remove(0) << "\n";
-
-  std::cout << "capacity: " << fibvec.capacity() << "\n";
-  std::cout << "count: " << fibvec.count() << "\n";
-  for (unsigned long i = 0; i < fibvec.count(); i++)
-  {
-    std::cout << " " << fibvec.lookup(i);
-  }
-  std::cout << "\n";
+  std::cout << "This program doesn't do anything interesting yet...\n";
 
   return 0;
 }
