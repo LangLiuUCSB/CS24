a1104817663e59d2eb95e67eef25d6433727515a
LangLiuUCSB <langliu@ucsb.edu>
2024-04-17 00:00:57 -0700

    for loop interators are now unsigned

--- labs/tictac/move-check.cpp
+++ labs/tictac/move-check.cpp
@@ -42,7 +42,7 @@ bool goodFormat(std::string input)
   {
     return false;
   }
-  for (int i = 7; input[i] != '#' && i < input.size(); i++)
+  for (unsigned int i = 7; input[i] != '#' && i < input.size(); i++)
   {
     if (!std::isspace(input[i]))
     {
