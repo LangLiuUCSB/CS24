0b88de41bc176bf6e450653fa95d8320b84f3dd2
LangLiuUCSB <langliu@ucsb.edu>
2024-04-17 21:41:00 -0700

    winning message typo

--- labs/tictac/game-check.cpp
+++ labs/tictac/game-check.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     }
     if (game.ended())
     {
-        std::cout << "Game over: " << game.prevPlayer << "wins.\n";
+        std::cout << "Game over: " << game.prevPlayer << " wins.\n";
     }
     else
     {
