9c69184a5833149bc1c6aaa871f2031e4a5cb988
LangLiuUCSB <langliu@ucsb.edu>
2024-05-07 03:57:22 -0700

    comments cleaning

--- labs/polish/AST.cpp
+++ labs/polish/AST.cpp
@@ -33,7 +33,6 @@ AST *AST::parse(const std::string &expression)
         AST *currNode;
         if (isArithOp(token))
         {
-            // std::cout << "ArithNode\n";
             if (stack->size() < 2)
             {
                 delete stack;
@@ -44,44 +43,34 @@ AST *AST::parse(const std::string &expression)
             switch (token[0])
             {
             case '+':
-                // std::cout << "+\n";
                 currNode = new ArithNode(ArithOp::Addition, operand1, operand2);
                 break;
             case '-':
-                // std::cout << "-\n";
                 currNode = new ArithNode(ArithOp::Subtraction, operand1, operand2);
                 break;
             case '*':
-                // std::cout << "*\n";
                 currNode = new ArithNode(ArithOp::Multiplication, operand1, operand2);
                 break;
             case '/':
-                // std::cout << "/\n";
                 currNode = new ArithNode(ArithOp::Division, operand1, operand2);
                 break;
             case '%':
-                // std::cout << "%\n";
                 currNode = new ArithNode(ArithOp::Modulus, operand1, operand2);
                 break;
             }
-            // std::cout << "push: " << currNode->value() << "\n";
         }
         else if (token == "~")
         {
-            // std::cout << "NotNode\n";
             if (stack->size() < 1)
             {
                 delete stack;
                 throw std::runtime_error("Not enough operands.");
             }
             currNode = new NotNode(stack->pop());
-            // std::cout << "push: " << currNode->value() << "\n";
         }
         else if (isDecimal(token))
         {
-            // std::cout << "FloatNode\n";
             currNode = new FloatNode(std::stod(token));
-            // std::cout << "push: " << currNode->value() << "\n";
         }
         else
         {
--- labs/polish/Stack.h
+++ labs/polish/Stack.h
@@ -2,8 +2,6 @@
 #define STACK_H
 
 #include "Nodes.h"
-// Use this file to define your Stack type.
-// Implement its member functions in Stack.cpp.
 
 class Stack
 {
