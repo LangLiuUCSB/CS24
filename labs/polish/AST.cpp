823940b3c0ddb192f71063dc6754cce5e425b59a
LangLiuUCSB <langliu@ucsb.edu>
2024-05-07 03:22:24 -0700

    .

--- labs/polish/AST.cpp
+++ labs/polish/AST.cpp
@@ -36,7 +36,7 @@ AST *AST::parse(const std::string &expression)
             // std::cout << "ArithNode\n";
             if (stack->size() < 2)
             {
-                delete stack;
+                //delete stack;
                 throw std::runtime_error("Not enough operands.");
             }
             AST *operand2 = stack->pop();
@@ -71,7 +71,7 @@ AST *AST::parse(const std::string &expression)
             // std::cout << "NotNode\n";
             if (stack->size() < 1)
             {
-                delete stack;
+                //delete stack;
                 throw std::runtime_error("Not enough operands.");
             }
             currNode = new NotNode(stack->pop());
