0b09106483da34aeaf49b0234f418aa2e142e45a
LangLiuUCSB <langliu@ucsb.edu>
2024-05-23 23:22:10 -0700

    32768

--- labs/counter/Counter.cpp
+++ labs/counter/Counter.cpp
@@ -1,7 +1,7 @@
 #include "Counter.h"
 
 // Counter Member Functions
-Counter::Counter() : numKeys(0), totalValue(0), numBuckets(16384)
+Counter::Counter() : numKeys(0), totalValue(0), numBuckets(32768)
 {
     buckets = new Bucket *[numBuckets];
     for (size_t i = 0; i < numBuckets; ++i)
