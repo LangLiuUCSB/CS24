7f46774fe7077ac4ef9c15e7ab52affd3d206377
LangLiuUCSB <langliu@ucsb.edu>
2024-06-10 15:28:50 -0700

    .

--- labs/voxhop/VoxMap.cpp
+++ labs/voxhop/VoxMap.cpp
@@ -127,7 +127,7 @@ VoxMap::VoxMap(std::istream &stream)
     }
     volume_below += map_area;
   }
-  frontiers = new OpenSet(map_volume >> 3);
+  frontiers = new OpenSet(map_volume / 8);
 }
 
 VoxMap::~VoxMap()
